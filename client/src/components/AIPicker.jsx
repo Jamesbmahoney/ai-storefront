2c85afd878cc6960d8db55d55ceee69dad967894
James Barry <jbm03003@gmail.com>
2023-05-04 20:00:23 -0400

    updating gitignore

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 
-import CustomButton from './CustomButton';
+import CustomButton from "./CustomButton";
 
-const AIPicker = () => {
-  return <div>AIPicker</div>;
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask the AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Ask The AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs"
+            />
+            <CustomButton
+              type="fill"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs"
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AIPicker;
