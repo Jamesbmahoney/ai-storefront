fb2eb17471680d7edfe26f799b0c8d288caff80e
James Barry <jbm03003@gmail.com>
2023-05-04 20:29:24 -0400

    fixing issues with API call

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -13,7 +13,7 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
-  res.status(200).json({ message: "HELLO FROM THE OTHER SIDE" });
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
 
 router.route("/").post(async (req, res) => {
@@ -21,7 +21,7 @@ router.route("/").post(async (req, res) => {
     const { prompt } = req.body;
 
     const response = await openai.createImage({
-      prompt: prompt,
+      prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
@@ -32,7 +32,7 @@ router.route("/").post(async (req, res) => {
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong!" });
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
